81bdd415af95f49806227d3e99e496ede80c9951
Ben Dosch <Benjamindosch@gmail.com>
2020-12-08 20:21:54 +0000

    accounting for -num and -num that end in 0

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,10 +2,15 @@
 import random
 number = random.randint(-10000, 10000)
 last = number % 10
-print('Last digit of ' + str(number) + ' is ' + str(last), end=' ')
-if last > 5:
-    print('and is greater than 5')
-elif last == 0:
+print('Last digit of ' + str(number) + ' is', end=' ')
+if number < 0 and last != 0:
+    print('-', end='')
+print(str(last), end=' ')
+if  last == 0:
     print('and is 0')
-else:
+elif number < 0:
+    print('and is less than 6 and not 0')
+elif last > 5:
+    print('and is greater than 5')
+elif last < 6 and last != 0:
     print('and is less than 6 and not 0')
