8fab7f2b731487573bfc7bf9ce967af25b176c7f
Ben Dosch <Benjamindosch@gmail.com>
2020-12-09 03:44:29 +0000

    13 added

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * insert_node - inserts a node with a number into a
+ * sorted list with the end being the bigest number
+ * @head: head of list
+ * @number: value of node to insert
+ * Return: address of new node, Null on fail
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *current = *head, *next, *new;
+
+	next = current->next;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+	if (number < current->n)
+	{
+		new->next = current;
+		return (new);
+	}
+	while (1)
+	{
+		if (number <= next->n && number >= current->n)
+		{
+			current->next = new;
+			new->next = next;
+			return (new);
+		}
+		else if (next->next == NULL)
+		{
+			next->next = new;
+			return (new);
+		}
+		else
+		{
+			current = current->next;
+			next = next->next;
+		}
+	}
+}
