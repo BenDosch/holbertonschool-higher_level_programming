67818456c801ddeccb81b0a8c51da0102a0b40f6
Ben Dosch <Benjamindosch@gmail.com>
2020-12-10 21:34:48 +0000

    removed space for 1 argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ def main():
     if len(argv) == 1:
         print("0 arguments.")
     elif len(argv) == 2:
-        print("1 argument:\n 1: {}".format(argv[1]))
+        print("1 argument:\n1: {}".format(argv[1]))
     else:
         print("{} arguments:".format(len(argv) - 1))
         for i in range(1, len(argv)):
