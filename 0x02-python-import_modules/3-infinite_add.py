f62bc1e1ca848896b6ccb815b966c4515a07288a
Ben Dosch <Benjamindosch@gmail.com>
2020-12-10 21:41:34 +0000

    ready to test

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def main():
+    import sys
+    argv = sys.argv
+    total = 0
+    for i in range(1, len(argv)):
+        total += int(argv[i])
+    print(total)
+
+if __name__ == "__main__":
+    main()
