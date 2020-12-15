5eb06d6e3c788b886ee3fdc63f307d0a93557d89
Ben Dosch <Benjamindosch@gmail.com>
2020-12-15 19:46:43 +0000

    ready to test

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < len(my_list) and idx >= 0:
+        return (my_list[idx])
+    return (None)
+
+
+def main():
+    test_list = [1, 2, 3, 4, 5]
+    idx = 3
+    print("Element at index {:d} is {}".format(idx, element_at(test_list, idx)))
+
+
+if __name__ == "__main__":
+    main()
