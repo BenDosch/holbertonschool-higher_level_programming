49266df1ad8434b9cde2112f78f4c485ab7b950e
Ben Dosch <Benjamindosch@gmail.com>
2020-12-15 19:30:48 +0000

    executable and added main function

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,3 +2,10 @@
 def print_list_integer(my_list=[]):
     for i in range(len(my_list)):
         print("{}".format(my_list[i]))
+
+def main():
+    test_list = [1, 2, 3, 4, 5]
+    print_list_integer(test_list)
+
+if __name__ == "__main__":
+    main()
