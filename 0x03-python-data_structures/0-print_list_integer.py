3b847f53b5ad1e6dd49d20750db6d37a70281e16
Ben Dosch <Benjamindosch@gmail.com>
2020-12-15 19:39:06 +0000

    pep8 fixes

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,9 +3,11 @@ def print_list_integer(my_list=[]):
     for i in range(len(my_list)):
         print("{:d}".format(my_list[i]))
 
+
 def main():
     test_list = [1, 2, 3, 4, 5]
     print_list_integer(test_list)
+
 
 if __name__ == "__main__":
     main()
