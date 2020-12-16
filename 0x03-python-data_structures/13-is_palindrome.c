f0041edb5b78b3903f99af819f247987e8e218b5
Ben Dosch <Benjamindosch@gmail.com>
2020-12-16 04:17:24 +0000

    2nd version using arry

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,7 +8,7 @@
 
 size_t list_len(const listint_t *h)
 {
-	unsigned long int i;
+	size_t i;
 
 	if (h == NULL)
 		return (0);
@@ -27,25 +27,25 @@ size_t list_len(const listint_t *h)
 int is_palindrome(listint_t **head)
 {
 	size_t len, i, j;
-	listint_t *start = *head, *end;
+	listint_t *start = *head;
+	int *nlist;
 
 	len = list_len(start);
 
 	if (len == 0 || len == 1 || !head || !start)
 		return (1);
-	printf("before first loop\n");
-	for (i = 0; i < (len / 2); i++, start = start->next)
-	{
-		printf("before second loop\n");
-		end = start;
-		for (j = 0; j < (len - (2 * i) - 1); j++)
+	nlist = malloc(sizeof(size_t) * len);
+
+	for (i = 0; i < len; i++, start = start->next)
+		nlist[i] = start->n;
+
+	for (i = 0, j = (len - 1); i < j; i++, j--)
+		if (nlist[i] != nlist[j])
 		{
-			printf("end: %d\n", end->n);
-			end = end->next;
-		}
-		printf("start: %d end: %d\n", start->n, end->n);
-		if (start->n != end->n)
+			free(nlist);
 			return (0);
-	}
+		}
+
+	free(nlist);
 	return (1);
 }
