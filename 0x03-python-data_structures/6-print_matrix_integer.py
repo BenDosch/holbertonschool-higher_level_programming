fe1ac3c592d0bd1963429979345059630b3c99fd
Ben Dosch <Benjamindosch@gmail.com>
2020-12-16 01:34:17 +0000

    ready to test

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        if len(matrix[i]) != len(matrix):
+            print("")
+            return
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end="")
+            if j < len(matrix[i]):
+                print(", ", end="")
+        print("")
+
+
+def main():
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+    print_matrix_integer(matrix)
+    print("--")
+    print_matrix_integer()
+
+
+if __name__ == "__main__":
+    main()
