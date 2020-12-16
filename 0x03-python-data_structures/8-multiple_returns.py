76b5c40ccd38ee048b63cc08e8caf7cee45999cf
Ben Dosch <Benjamindosch@gmail.com>
2020-12-16 02:33:01 +0000

    ready to test

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        info = (0, None)
+        return info
+    else:
+        info = (len(sentence), sentence[0])
+        return info
+
+
+def main():
+    sentence = "At Holberton school, I learnt C!"
+    length, first = multiple_returns(sentence)
+    print("Length: {:d} - First character: {}".format(length, first))
+
+
+if __name__ == "__main__":
+    main()
