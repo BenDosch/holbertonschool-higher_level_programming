c3f9c0977e318fe6b6a86b9cefb79f21109e7a0d
Ben Dosch <Benjamindosch@gmail.com>
2020-12-16 00:32:23 +0000

    and to or

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    new_list = [x for x in my_list]
-    if idx < 0 or idx >= len(my_list):
-        return new_list
-    else:
-        new_list[idx] = element
-        return new_list
-
-
-"""def replace_in_list(my_list, idx, element):
+def replace_in_list(my_list, idx, element):
     other_list = [x for x in my_list]
-    if idx >= len(my_list) and idx < 0:
+    if idx >= len(my_list) or idx < 0:
         return other_list
     other_list[idx] = element
     return other_list
@@ -27,4 +18,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-"""
