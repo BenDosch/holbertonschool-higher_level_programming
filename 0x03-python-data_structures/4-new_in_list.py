22b46ee222b9b733f8b06a51a23433c827b603be
Ben Dosch <Benjamindosch@gmail.com>
2020-12-16 00:54:51 +0000

    main reinstated

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -7,3 +7,15 @@ def new_in_list(my_list, idx, element):
     if idx < len(my_list) and idx >= 0:
         copy_list[idx] = element
     return (copy_list)
+
+def main():
+    test_list = [1, 2, 3, 4, 5]
+    idx = 3
+    new_element = 9
+    new_list = replace_in_list(test_list, idx, new_element)
+    print(new_list)
+    print(test_list)
+
+
+if __name__ == "__main__":
+    main()
