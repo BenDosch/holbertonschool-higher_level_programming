718c3e00ce95b76bb9ecf3f41123c3e8f085757c
Ben Dosch <Benjamindosch@gmail.com>
2020-12-16 00:35:06 +0000

    no main()

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,16 +6,3 @@ def replace_in_list(my_list, idx, element):
     else:
         other_list[idx] = element
         return other_list
-
-
-def main():
-    test_list = [1, 2, 3, 4, 5]
-    idx = 3
-    new_element = 9
-    new_list = replace_in_list(test_list, idx, new_element)
-    print(new_list)
-    print(test_list)
-
-
-if __name__ == "__main__":
-    main()
