39e439152eb19b91ee693cb5b11c28f803312ec7
Ben Dosch <Benjamindosch@gmail.com>
2020-12-16 00:33:56 +0000

    using an else

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,8 +3,9 @@ def replace_in_list(my_list, idx, element):
     other_list = [x for x in my_list]
     if idx >= len(my_list) or idx < 0:
         return other_list
-    other_list[idx] = element
-    return other_list
+    else:
+        other_list[idx] = element
+        return other_list
 
 
 def main():
