296583b30d988a7204e59a2dc65bed4e8c734e37
Ben Dosch <Benjamindosch@gmail.com>
2020-12-16 02:59:36 +0000

    ready to test

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    bool_list = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2:
+            bool_list.append(False)
+        else:
+            bool_list.append(True)
+    return bool_list
+
+
+def main():
+    my_list = [0, 1, 2, 3, 4, 5, 6]
+    list_r = divisible_by_2(my_list)
+
+    i = 0
+    while i < len(list_r):
+        if list_r[i]:
+            print("{:d} {:s} divisible by 2".format(my_list[i], "is"))
+        else:
+            print("{:d} {:s} divisible by 2".format(my_list[i], "is not"))
+        i += 1
+
+
+if __name__ == "__main__":
+    main()
