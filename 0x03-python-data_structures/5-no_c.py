dd4a725c3238b364699cd189a5f824da42ef9df9
Ben Dosch <Benjamindosch@gmail.com>
2020-12-16 01:08:46 +0000

    ready to test

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for i in range(len(my_string)):
+        if my_string[i] == "c" or my_string[i] == "C":
+            continue
+        else:
+            new_string += my_string[i]
+    return new_string
+
+
+def main():
+    print(no_c("Holberton School"))
+    print(no_c("Chicago"))
+    print(no_c("C is fun!"))
+
+
+if __name__ == "__main__":
+    main()
