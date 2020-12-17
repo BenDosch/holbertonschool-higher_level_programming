a26a9e2233c2d8f801b9ebe297849a5516b69cdb
Ben Dosch <Benjamindosch@gmail.com>
2020-12-17 20:58:26 +0000

    ready to test

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
 
+
+def print_sorted_dictionary(a_dictionary):
+    keys = list(a_dictionary.keys())
+    keys.sort()
+    [print("{}: {}".format(key, a_dictionary[key])) for key in keys]
 
 
 def main():
-    from 6-print_sorted_dictionary import print_sorted_dictionary
-    print_sorted_dictionary = 6-print_sorted_dictionary.print_sorted_dictionary
-    a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
+    a_dictionary = {'language': "C", 'number': 89, 'track': "Low level"}
     new_dict = update_dictionary(a_dictionary, 'language', "Python")
     print_sorted_dictionary(new_dict)
     print("--")
     print_sorted_dictionary(a_dictionary)
-
     print("--")
     print("--")
-
     new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
     print_sorted_dictionary(new_dict)
     print("--")
