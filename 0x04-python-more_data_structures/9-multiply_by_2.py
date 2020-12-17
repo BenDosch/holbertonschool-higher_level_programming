845b5ee004a096a1b47742c2b52c98c14ed04ee3
Ben Dosch <Benjamindosch@gmail.com>
2020-12-17 21:27:42 +0000

    ready to test

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    new_dict = {}
+    for key in a_dictionary:
+        new_dict[key] = a_dictionary[key] * 2
+    return new_dict
 
+
+def print_sorted_dictionary(a_dictionary):
+    keys = list(a_dictionary.keys())
+    keys.sort()
+    [print("{}: {}".format(key, a_dictionary[key])) for key in keys]
 
 
 def main():
-    from 6-print_sorted_dictionary import print_sorted_dictionary
-
     a_dictionary = {'John': 12, 'Alex': 8, 'Bob': 14, 'Mike': 14, 'Molly': 16}
     new_dict = multiply_by_2(a_dictionary)
     print_sorted_dictionary(a_dictionary)
