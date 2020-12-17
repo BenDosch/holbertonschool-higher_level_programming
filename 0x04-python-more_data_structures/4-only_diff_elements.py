2898bd7ea09728cd5780d562dd2f6ebc6f25c2c9
Ben Dosch <Benjamindosch@gmail.com>
2020-12-17 20:21:28 +0000

    ready to test

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-
+    return set(set_1 ^ set_2)
 
 
 def main():
-    set_1 = { "Python", "C", "Javascript" }
-    set_2 = { "Bash", "C", "Ruby", "Perl" }
+    set_1 = {"Python", "C", "Javascript"}
+    set_2 = {"Bash", "C", "Ruby", "Perl"}
     od_set = only_diff_elements(set_1, set_2)
     print(sorted(list(od_set)))
 
