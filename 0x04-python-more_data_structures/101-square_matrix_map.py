12c6ffd5588eeca95e88aa81e543ee209146bf09
Ben Dosch <Benjamindosch@gmail.com>
2020-12-18 02:18:46 +0000

    ready to test

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,18 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-
-
-
-def main():
-    matrix = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-    ]
-    new_matrix = square_matrix_map(matrix)
-    print(new_matrix)
-    print(matrix)
-
-
-if __name__ == "__main__":
-    main()
+    return list(map(lambda x: list(map(lambda y: y * y, x)), matrix))
