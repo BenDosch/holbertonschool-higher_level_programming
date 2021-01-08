328a27fe20cf4f882bd0ec03afdc33fac0cfd38b
Ben Dosch <Benjamindosch@gmail.com>
2021-01-08 21:58:32 +0000

    task 3 added

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A module that contains a function that prints a swaure of #'s
+"""
+
+
+def print_square(size):
+    """Function that prints a squre of #'s
+
+    Args:
+        size (int): size of square
+
+    """
+
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    else:
+    for range(0, size):
+        print("{}".format("#" * size))
