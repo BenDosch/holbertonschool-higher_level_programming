db553d39fe81d244d6dab49f8ab3d67b09dd70a9
Ben Dosch <Benjamindosch@gmail.com>
2021-01-11 18:30:37 +0000

    task 0 ready

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module
+"""
+
+
+class Rectangle():
+    """Class that defines a rectangle"""
+
+
+if __name__ == "__main__":
+    my_rectangle = Rectangle()
+    print(type(my_rectangle))
+    print(my_rectangle.__dict__)
