da47e25a311de26f5f7383acc8b81bdf53ab069c
Ben Dosch <Benjamindosch@gmail.com>
2021-01-12 00:23:53 +0000

    pep8 fixes

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -35,9 +35,9 @@ class Rectangle():
         if self.perimeter() == 0:
             return ""
         else:
-            return (((("{}".format(self.print_symbol)) * self.width
-                       + "\n") * (self.height)
-                     + ("{}".format(self.print_symbol) * self.width)))
+            return (((("{}".format(self.print_symbol)) * self.width + "\n") *
+                    (self.height - 1) + ("{}".format(self.print_symbol) *
+                    self.width)))
 
     def __repr__(self):
         """Method that returns a string representation of the Rectangle
@@ -124,14 +124,16 @@ if __name__ == "__main__":
     my_rectangle_1 = Rectangle(8, 4)
     my_rectangle_2 = Rectangle(2, 3)
 
-    if my_rectangle_1 is Rectangle.bigger_or_equal(my_rectangle_1, my_rectangle_2):
+    if my_rectangle_1 is Rectangle.bigger_or_equal(my_rectangle_1,
+                                                   my_rectangle_2):
         print("my_rectangle_1 is bigger or equal to my_rectangle_2")
     else:
         print("my_rectangle_2 is bigger than my_rectangle_1")
 
     my_rectangle_2.width = 10
     my_rectangle_2.height = 5
-    if my_rectangle_1 is Rectangle.bigger_or_equal(my_rectangle_1, my_rectangle_2):
+    if my_rectangle_1 is Rectangle.bigger_or_equal(my_rectangle_1,
+                                                   my_rectangle_2):
         print("my_rectangle_1 is bigger or equal to my_rectangle_2")
     else:
         print("my_rectangle_2 is bigger than my_rectangle_1")
