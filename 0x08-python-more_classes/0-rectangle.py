a80c0f9cd2f37dc2bd90f89dae58f511b4df8729
Ben Dosch <Benjamindosch@gmail.com>
2021-01-12 00:04:10 +0000

    pep8 fixes

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 """Module that contains the class Rectangle"""
-"""
 
 
 class Rectangle():
     """Class that defines a rectangle"""
-
-
-if __name__ == "__main__":
-    my_rectangle = Rectangle()
-    print(type(my_rectangle))
-    print(my_rectangle.__dict__)
