7a49edb29761133aceff8cda7479d70de2b7e3e3
Ben Dosch <Benjamindosch@gmail.com>
2021-01-20 01:28:17 +0000

    main split into diffrent file

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -17,19 +17,3 @@ def add_attribute(obj, a_name, a_value):
         setattr(obj, a_name, a_value)
     else:
         raise TypeError("can't add new attribute")
-
-
-if __name__ == "__main__":
-    class MyClass():
-        pass
-
-    mc = MyClass()
-    add_attribute(mc, "name", "John")
-    print(mc.name)
-
-    try:
-        a = "My String"
-        add_attribute(a, "name", "Bob")
-        print(a.name)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
