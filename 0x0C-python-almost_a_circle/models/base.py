b0f71a3f8404a92218f219f2a89df887bd3e9cc2
Ben Dosch <Benjamindosch@gmail.com>
2021-01-27 17:19:52 +0000

    fixed behavior for save_to_file(None)

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -71,11 +71,13 @@ class Base():
         """
 
         with open(cls.__name__ + ".json", mode="w") as f:
-
             new_list = []
-            for obj in list_objs:
-                new_list.append(obj.to_dictionary())
-            f.write(cls.to_json_string(new_list))
+            if list_objs is None:
+                f.write(new_list)
+            else:
+                for obj in list_objs:
+                    new_list.append(obj.to_dictionary())
+                f.write(cls.to_json_string(new_list))
 
     @classmethod
     def create(cls, **dictionary):
