42331608fd3bbb782d0df155f44e3f1ec54ab634
Ben Dosch <Benjamindosch@gmail.com>
2021-03-01 20:49:10 +0000

    advanced tasks addded, 102 still in progress

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,9 @@
--- Script to
--- Querry to 
-
+-- Script to convert "hbtn_0c_0" database to UTF8
+-- Querry to select databse
+USE hbtn_0c_0;
+-- Querry to convert "hbtn_0c_0" database to UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Querry to convert "first_table" table to UTF8 
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Querry to convert column "name" to UTF8
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,4 @@
--- Script to
--- Querry to 
-
+-- Script to display the average temperature (Fahrenheit) by city ordered by temperature (descending).
+-- Querry to the average temperature (Fahrenheit) by city ordered by temperature (descending).
+SELECT city, AVG(value) AS avg_temp FROM temperatures
+GROUP BY city ORDER BY avg_temp DESC;
