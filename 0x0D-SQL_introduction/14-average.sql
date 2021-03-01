91ade28a8f1b09501af3d5538ebe6f58a40134ce
Ben Dosch <Benjamindosch@gmail.com>
2021-03-01 20:21:20 +0000

    spelling mistake

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- Script to compute the score average of all records in the table "second_table" of the database provided.
 -- Querry to compute the score average of all records in the table "second_table" of the database provided.
-SELECT AVG(score) AS "avrage" FROM second_table;
+SELECT AVG(score) AS "average" FROM second_table;
