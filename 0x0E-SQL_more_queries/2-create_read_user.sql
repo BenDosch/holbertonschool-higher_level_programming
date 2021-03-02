c17b9e8b63cc0b7378a7ae0cbd089d4d0ef58d21
Ben Dosch <Benjamindosch@gmail.com>
2021-03-02 19:40:49 +0000

    first 6 tasks ready to check

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,7 @@
--- Script that
--- Query that 
-
+-- Script that creates the database hbtn_0d_2 and the user user_0d_2.
+-- Query that creates the databse
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- Query that creates the user
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+-- Query that sets password for user. 
+SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,6 @@
--- Script that
--- Query that 
-
+-- Script that creates the table id_not_null in database provided.
+-- Query that creates the table.
+CREATE TABLE IF NOT EXISTS id_not_null(
+    id INT DEFAULT 1,
+    name VARCHAR(256)
+);
