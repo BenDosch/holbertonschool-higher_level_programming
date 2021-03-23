d6577af91afaaae93ec831880e0d33182a08348f
Ben Dosch <Benjamindosch@gmail.com>
2021-03-23 23:26:40 +0000

    task 12 ready to check

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const dict = require('./101-data.js').dict;
+const newDict = {};
+for (const key in dict) {
+  if (!(dict[key] in newDict)) {
+    newDict[dict[key]] = [];
+  }
+  const list = newDict[dict[key]];
+  list.push(key);
+}
+console.log(newDict);
