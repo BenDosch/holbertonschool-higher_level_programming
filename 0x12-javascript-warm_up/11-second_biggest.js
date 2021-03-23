09aca82cf78959eb054bb8baf2c50b7343c0a1bb
Ben Dosch <Benjamindosch@gmail.com>
2021-03-23 01:21:09 +0000

    semistandard fixes

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@ const ary = process.argv;
 ary.shift();
 ary.shift();
 if (ary[1]) {
-  ary.sort(function(a, b){return b - a});
+  ary.sort(function (a, b) { return b - a; });
   console.log(ary[1]);
 } else {
   console.log(0);
