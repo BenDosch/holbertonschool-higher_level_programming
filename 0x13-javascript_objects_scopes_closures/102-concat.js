d35d8f320732bcdb6842f3bca8865f832293b7da
Ben Dosch <Benjamindosch@gmail.com>
2021-03-24 01:41:15 +0000

    task 13 added

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,27 @@
 #!/usr/bin/node
+const fs = require('fs');
+const src1 = process.argv[2];
+const src2 = process.argv[3];
+const dest = process.argv[4];
+const a = fs.readFileSync(src1, { encoding: 'utf8', flag: 'r' },
+  function (err) {
+    if (err) {
+      return console.error(err);
+    }
+  });
+const b = fs.readFileSync(src2, { encoding: 'utf8', flag: 'r' },
+  function (err) {
+    if (err) {
+      return console.error(err);
+    }
+  });
+fs.writeFile(dest, a, function (err) {
+  if (err) {
+    return console.error(err);
+  }
+});
+fs.appendFile(dest, '\n' + b, function (err) {
+  if (err) {
+    return console.error(err);
+  }
+});
