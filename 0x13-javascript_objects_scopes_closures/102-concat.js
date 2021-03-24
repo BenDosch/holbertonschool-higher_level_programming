773efbbfb9bac2ea1e1ecb6a94eaf216ba2e1661
Ben Dosch <Benjamindosch@gmail.com>
2021-03-24 01:55:49 +0000

    added newline at end of file

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,5 +2,5 @@
 const fs = require('fs');
 fs.writeFileSync(process.argv[4],
   fs.readFileSync(process.argv[2]) + '\n' +
-  fs.readFileSync(process.argv[3])
+  fs.readFileSync(process.argv[3]) + '\n'
 );
