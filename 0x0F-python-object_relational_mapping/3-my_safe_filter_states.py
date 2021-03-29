7ca7cee227799f04c259894f45b56ad6a09e0d35
Ben Dosch <Benjamindosch@gmail.com>
2021-03-29 03:05:16 +0000

    prevent use of escaped quotes

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,15 +8,16 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(
-        host="localhost", port=3306, user=sys.argv[1],
-        passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states \
-                Where name = %s \
-                ORDER BY id ASC", (sys.argv[4],))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    conn.close()
+    if !(sys.argv[4].fid("\'")) and !(sys.argv[4].find('\"')):
+        conn = MySQLdb.connect(
+            host="localhost", port=3306, user=sys.argv[1],
+            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM states \
+                    Where name = %s \
+                    ORDER BY id ASC", (sys.argv[4],))
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        conn.close()
