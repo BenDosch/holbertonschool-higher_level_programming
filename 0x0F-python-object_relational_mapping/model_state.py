6d5fd26ceff630153593a44bbabb31af8165375a
Ben Dosch <Benjamindosch@gmail.com>
2021-03-29 03:40:45 +0000

    added missing docstrings

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Module containing the class State"""
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -6,6 +8,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """Class that describes a state"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True,
                 unique=True, autoincrement=True, nullable=False)
