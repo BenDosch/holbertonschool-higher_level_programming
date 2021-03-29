4c91c1bdadf61e53d95b48d9ef22abf8fd41f63f
Ben Dosch <Benjamindosch@gmail.com>
2021-03-29 03:39:35 +0000

    put back both autoincrement and unique

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,5 +7,6 @@ Base = declarative_base()
 
 class State(Base):
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True,
+                unique=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
