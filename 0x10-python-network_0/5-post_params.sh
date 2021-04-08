8e0b645bd2b959132188a9030b25ffdcc8eeec7b
Ben Dosch <Benjamindosch@gmail.com>
2021-04-08 22:24:33 +0000

    task 5 ready to test

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Comment
-
-Code goes here
+# script that takes in a URL, sends a POST request to the passed URL
+crul -sd "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
