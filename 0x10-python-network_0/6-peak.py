13add3319f4239e9db885798716bf1a3dc5dabb3
Ben Dosch <Benjamindosch@gmail.com>
2021-04-08 20:45:30 +0000

    directory set up, task 0 finshed

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Module containing function find_peak """
+
+def find_peak(list_of_integers):
+    """Finction that  finds a peak in a list of unsorted integers."""
+
+if __name__ == "__main__":
+    print(find_peak([1, 2, 4, 6, 3]))
+    print(find_peak([4, 2, 1, 2, 3, 1]))
+    print(find_peak([2, 2, 2]))
+    print(find_peak([]))
+    print(find_peak([-2, -4, 2, 1]))
+    print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
---
+++ 0x10-python-network_0/README.md
@@ -0,0 +1,4 @@
+# 0x10-python-network_0
+
+## Intro
+Directory containing holberton python networking projects and bash scripts
