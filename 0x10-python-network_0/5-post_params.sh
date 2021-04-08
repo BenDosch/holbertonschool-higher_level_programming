0ca93fb2ea89d603dff90d30171acdd91d093b3b
Ben Dosch <Benjamindosch@gmail.com>
2021-04-08 22:32:01 +0000

    adding in the extra newline

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a POST request to the passed URL
-crul -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+crul -sd "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
