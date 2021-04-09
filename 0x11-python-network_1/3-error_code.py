3e981ff9ba33f1a15ae36200be8dcb564b5984ba
Ben Dosch <Benjamindosch@gmail.com>
2021-04-09 20:10:18 +0000

    task 3 ready to check

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,2 +1,18 @@
-#/usr/bin/python3.4
-"""Doctstring goes here"""
+#!/usr/bin/python3.4                                                                            
+"""Script that takes in a URL, sends a request to the URL and                                   
+displays the body of the response"""
+import urllib.request
+import urllib.error
+from sys import argv
+
+
+if __name__ == "__main__":
+    if (argv[1] is not None):
+        url = argv[1]
+        try:
+            response = urllib.request.urlopen(url)
+            body = response.read()
+            body = body.decode("UTF-8")
+            print(body)
+        except urllib.error.HTTPError as e:
+            print("Error code: {}".format(e.code))
