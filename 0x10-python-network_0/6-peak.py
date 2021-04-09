5e2074b169797d019f4fb4afcec77ddf85091412
Ben Dosch <Benjamindosch@gmail.com>
2021-04-09 01:37:49 +0000

    task 6 ready to check

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 """ Module containing function find_peak """
 
+
 def find_peak(list_of_integers):
     """Finction that  finds a peak in a list of unsorted integers."""
+    begining = 0
+    mid = 0
+    end = len(list_of_integers) - 1
+    if end > 0:
+        while begining < end:
+            mid = begining + (end - begining) // 2
+            if list_of_integers[mid] > list_of_integers[mid - 1] and
+            list_of_integers[mid] > list_of_integers[mid + 1]:
+                return list_of_integers[mid]
+            if list_of_integers[mid - 1] > list_of_integers[mid + 1]:
+                end = mid
+            else:
+                begining = mid + 1
+        return list_of_integers[begining]
 
 if __name__ == "__main__":
     print(find_peak([1, 2, 4, 6, 3]))
