9d02e5882ff0acc9210ec237af7f1330031a7964
Ben Dosch <Benjamindosch@gmail.com>
2021-04-09 21:05:12 +0000

    task 6 ready to check

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,2 +1,18 @@
-#/usr/bin/python3.4
-"""Doctstring goes here"""
+#!/usr/bin/python3.4
+"""Script that takes in a URL and an email address, sends a POST request
+to the passed URL with the email as a parameter, and finally displays
+the body of the response."""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    if argv[1] is not None and argv[2] is not None:
+        url = argv[1]
+        email = argv[2]
+        url = argv[1]
+        email = argv[2]
+        data = {'email': email}
+        r = requests.post(url, data)
+        body = r.text
+        print(str(body))
