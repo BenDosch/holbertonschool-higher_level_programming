64f9f3c29179da1fb8e1fd60160df32c98bc1da7
Ben Dosch <Benjamindosch@gmail.com>
2021-04-09 19:56:05 +0000

    task 2 ready to check

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,2 +1,19 @@
-#/usr/bin/python3.4
-"""Doctstring goes here"""
+#!/usr/bin/python3.4
+"""Script that takes in a URL and an email, sends a POST request to the passed
+URL with the email as a parameter, and displays the body of the response"""
+import urllib.request
+import urllib.parse
+from sys import argv
+
+
+if __name__ == "__main__":
+    if (argv[1] is not None and argv[2] is not None):
+        url = argv[1]
+        email = argv[2]
+        values = {'email': email}
+        data = urllib.parse.urlencode(values)
+        data = data.encode('ascii')  # data should be bytes
+        with urllib.request.urlopen(url, data) as response:
+            body = response.read()
+            body = body.decode("UTF-8")
+            print(body)
