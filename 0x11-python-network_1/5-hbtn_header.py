260e0b98f2980541c5af0d90197f8039c8f7b437
Ben Dosch <Benjamindosch@gmail.com>
2021-04-09 20:51:30 +0000

    task 5 ready to check

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,2 +1,12 @@
-#/usr/bin/python3.4
-"""Doctstring goes here"""
+#!/usr/bin/python3.4
+"""Script that takes in a URL, sends a request to the URL and displays the
+value of the variable X-Request-Id in the response header"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    r = requests.get(url)
+    header = r.headers
+    print(header.get("X-Request-Id"))
