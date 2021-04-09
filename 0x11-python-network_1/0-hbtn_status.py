b37c04213f83b59c8743be0d910e2798918d35c3
Ben Dosch <Benjamindosch@gmail.com>
2021-04-09 19:03:39 +0000

    task 0 ready to check

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,2 +1,13 @@
-#/usr/bin/python3.4
-"""Doctstring goes here"""
+#!/usr/bin/python3.4
+"""Script that fetches https://intranet.hbtn.io/status"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: " + str(type(content)))
+        print("\t- content: " + str(content))
+        content = content.decode("UTF-8")
+        print("\t- utf8 content: " + content)
