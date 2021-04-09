c35cab930bf65fec16010f5d882b8c4aed944343
Ben Dosch <Benjamindosch@gmail.com>
2021-04-09 20:38:07 +0000

    task 4 ready to check

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,2 +1,12 @@
-#/usr/bin/python3.4
-"""Doctstring goes here"""
+#!/usr/bin/python3.4
+"""Sccript that fetches https://intranet.hbtn.io/status"""
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get("https://intranet.hbtn.io/status")
+    content = r.text
+    print("Body response:")
+    print("\t- type: " + str(type(content)))
+    print("\t- content: " + str(content))
+    
