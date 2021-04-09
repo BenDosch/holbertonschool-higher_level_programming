0ffd780b54c833e395e024edd4f6e8a18ca45601
Ben Dosch <Benjamindosch@gmail.com>
2021-04-09 20:13:47 +0000

    trying with out the check on argv

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,12 +7,11 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    if (argv[1] is not None):
-        url = argv[1]
-        try:
-            response = urllib.request.urlopen(url)
-            body = response.read()
-            body = body.decode("UTF-8")
-            print(body)
-        except urllib.error.HTTPError as e:
-            print("Error code: {}".format(e.code))
+    url = argv[1]
+    try:
+        response = urllib.request.urlopen(url)
+        body = response.read()
+        body = body.decode("UTF-8")
+        print(body)
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
