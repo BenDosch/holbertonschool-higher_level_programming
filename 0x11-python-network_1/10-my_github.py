67d875c09c24ff3ff5f3ab6e774b4d0db44db9d7
Ben Dosch <Benjamindosch@gmail.com>
2021-04-09 22:45:53 +0000

    task 10 ready to check

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,2 +1,19 @@
-#/usr/bin/python3.4
-"""Doctstring goes here"""
+#!/usr/bin/python3.4
+"""Script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    if len(argv) > 2:
+        user = argv[1]
+        token = argv[2]
+        query_url = "https://api.github.com/users/{}".format(user)
+        params = {
+            "state": "open",
+        }
+        headers = {'Authorization': 'token {}'.format(token)}
+        r = requests.get(query_url, headers=headers, params=params)
+        j = r.json()
+        print(j.get("id"))
