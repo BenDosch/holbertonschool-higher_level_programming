0ef6a8528b2f0def4faceb0307b594881d7b6796
Ben Dosch <Benjamindosch@gmail.com>
2021-05-04 18:17:23 +0000

    directory set up and task 0 ready to check

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const myArgs = process.argv.slice(2);
+fs.readFile(myArgs[0], "utf-8", (err, data) => {
+    if (err) { console.log(err) }
+    console.log(data);
+});
---
+++ 0x14-javascript-web_scraping/README.md
@@ -0,0 +1,4 @@
+# 0x14-javascript-web_scraping
+
+## Intro
+Directory containing Holberton school projects on web scraping with javascript
