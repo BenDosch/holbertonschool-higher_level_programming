060fee335833de37c27f19c84dda8239cecf305a
Ben Dosch <Benjamindosch@gmail.com>
2021-05-04 19:59:14 +0000

    frist advanced task ready to check

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,27 @@
 #!/usr/bin/node
+
+const request = require('request');
+const myArgs = process.argv.slice(2);
+const options = {
+  url: 'https://swapi-api.hbtn.io/api/films/' + myArgs[0],
+  method: 'GET',
+  headers: {
+    'Accept-Charset': 'utf-8'
+  }
+};
+request(options, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const chars = (JSON.parse(body).characters);
+    chars.forEach(urlPage => {
+      request(urlPage, (error, response, body) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    });
+  }
+});
