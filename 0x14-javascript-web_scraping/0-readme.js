8c114dc58b76a6b9ea7578c8ca4a8fb3c8e15e2d
Ben Dosch <Benjamindosch@gmail.com>
2021-05-04 18:18:16 +0000

    semistandard fixes

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const myArgs = process.argv.slice(2);
-fs.readFile(myArgs[0], "utf-8", (err, data) => {
-    if (err) { console.log(err) }
-    console.log(data);
+fs.readFile(myArgs[0], 'utf-8', (err, data) => {
+  if (err) { console.log(err); }
+  console.log(data);
 });
