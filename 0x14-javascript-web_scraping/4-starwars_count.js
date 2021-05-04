b75d988a33d98de02d2d4f282758779315bcd5ad
Ben Dosch <Benjamindosch@gmail.com>
2021-05-04 19:18:03 +0000

    task 4 done

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,28 @@
 #!/usr/bin/node
+
+const request = require('request');
+const myArgs = process.argv.slice(2);
+const options = {
+  url: myArgs[0],
+  method: 'GET',
+  headers: {
+    'Accept-Charset': 'utf-8'
+  }
+};
+request(options, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const movies = JSON.parse(body).results;
+    let total = 0;
+    for (const i in movies) {
+      const people = movies[i].characters;
+      for (const j in people) {
+        if (people[j].includes('18')) {
+          total++;
+        }
+      }
+    }
+    console.log(total);
+  }
+});
