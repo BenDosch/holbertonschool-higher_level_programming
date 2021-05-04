94d3548f57a59623b36f48cb9d9719fcc425f957
Ben Dosch <Benjamindosch@gmail.com>
2021-05-04 19:28:31 +0000

    task 5 ready to check

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+const myArgs = process.argv.slice(2);
+const options = {
+  url: myArgs[0],
+  method: 'GET',
+  headers: {
+    'Accept-Charset': 'utf-8'
+  }
+};
+request(options, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(myArgs[1], body, 'utf-8', (err, data) => {
+      if (err) console.log(err);
+    });
+  }
+});
