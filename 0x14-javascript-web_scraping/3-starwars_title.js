b247439e16f3e2801628a5f6ca50e45b2cd80237
Ben Dosch <Benjamindosch@gmail.com>
2021-05-04 19:01:19 +0000

    task 3 ready to check

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+
+const request = require('request');
+const myArgs = process.argv.slice(2);
+const options = {
+  url: 'https://swapi-api.hbtn.io/api/films/' + myArgs[0],
+  method: 'GET',
+  headers: {
+    'Accept-Charset': 'utf-8'
+  }
+};
+request(options, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
