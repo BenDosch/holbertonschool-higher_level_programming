c613b928b247d42620f89df4020aea4236453233
Ben Dosch <Benjamindosch@gmail.com>
2021-05-04 19:48:41 +0000

    task 6 ready to checkgp

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,29 @@
 #!/usr/bin/node
+
+const request = require('request');
+const myArgs = process.argv.slice(2);
+const options = {
+  url: myArgs[0],
+  method: 'GET',
+  headers: {
+    'Accept-Charset': 'utf-8'
+  }
+};
+const comp = {};
+request(options, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const tasks = JSON.parse(body);
+    for (const i in tasks) {
+      if (tasks[i].completed) {
+        if (comp[tasks[i].userId]) {
+          comp[tasks[i].userId] += 1;
+        } else {
+          comp[tasks[i].userId] = 1;
+        }
+      }
+    }
+  }
+  console.log(comp);
+});
